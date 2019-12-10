db6c531bc675b129c0a08e650bad608a483b6b4a
mattia solarino <mattiasolarino@email.it>
2019-12-10 15:56:29 +0100

    finished with the backend fo the map + all index

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -1,7 +1,15 @@
 class CentersController < ApplicationController
-
   def index
-    @centers = Center.all
+    @centers = Center.geocoded
+
+    @markers = @centers.map do |center|
+      {
+        lat: center.latitude,
+        lng: center.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { center: center }),
+        image_url: helpers.asset_url('circle-cropped.png')
+      }
+    end
   end
 
   def show
