434f50f9dcec00ee1241462d79be70a41008ccdd
Omamerhi Etefia <omamerhie@gmail.com>
2019-12-10 19:03:42 +0100

    Added owner/customer references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_161750) do
+ActiveRecord::Schema.define(version: 2019_12_10_170416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_12_10_161750) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id"
+    t.index ["customer_id"], name: "index_bookings_on_customer_id"
     t.index ["treatment_id"], name: "index_bookings_on_treatment_id"
   end
 
@@ -35,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_12_10_161750) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "owner_id"
+    t.index ["owner_id"], name: "index_centers_on_owner_id"
   end
 
   create_table "treatments", force: :cascade do |t|
