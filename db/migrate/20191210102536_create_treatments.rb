afcaf0760127707ce7c294c1dfe4204d337f8839
Omamerhi Etefia <omamerhie@gmail.com>
2019-12-10 11:27:01 +0100

    Generated the treatment model and controller

---
+++ db/migrate/20191210102536_create_treatments.rb
@@ -0,0 +1,15 @@
+class CreateTreatments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :treatments do |t|
+      t.string :name
+      t.text :description
+      t.integer :price_cents
+      t.string :category
+      t.string :photo
+      t.integer :capcacity_per_hour
+      t.integer :duration
+
+      t.timestamps
+    end
+  end
+end
