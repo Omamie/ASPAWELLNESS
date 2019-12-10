1225b469b40ac61eb7a91c1eaafc2d372004e27d
alex spacagna alessandro.spacagna@libero.it <alessandro.spacagna@libero.it>
2019-12-10 18:20:55 +0100

    'im still working on the index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources :centers, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :centers, only: [:show, :index]
+  resources :centers, only: [:show, :index]
+
+  resources :searches, only: :index, path: :search
 end
