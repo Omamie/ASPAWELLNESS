b534edd5063c6565617303797c1ab53385177099
Omamerhi Etefia <omamerhie@gmail.com>
2019-12-10 17:10:52 +0100

    Created the Owner/Customer type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_103711) do
+ActiveRecord::Schema.define(version: 2019_12_10_155050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,12 +41,10 @@ ActiveRecord::Schema.define(version: 2019_12_10_103711) do
     t.integer "price_cents"
     t.string "category"
     t.string "photo"
-    t.integer "capcacity_per_hour"
+    t.integer "capacity_per_hour"
     t.integer "duration"
-    t.bigint "center_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["center_id"], name: "index_treatments_on_center_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,10 +55,14 @@ ActiveRecord::Schema.define(version: 2019_12_10_103711) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.string "address"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "treatments", "centers"
-
+  add_foreign_key "bookings", "treatments"
 end
