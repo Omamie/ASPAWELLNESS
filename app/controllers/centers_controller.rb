6ab81f86bdcc161caff2cff96652620b1aec8cbf
mattia solarino <mattiasolarino@email.it>
2019-12-11 11:18:32 +0100

    merging new master

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -15,11 +15,11 @@ class CentersController < ApplicationController
   def show
     @center = Center.find(params[:id])
   end
-  def index
 
+  def index
     if params[:treatment].present? && [:address].present?
-       sql_query = "name ILIKE :treatment and address ILIKE :address"
-       @treatments = Treatment.where(sql_query, treatment: "%#{params[:treatment]}%" , address:"%#{params[:address]}%")
+      sql_query = "name ILIKE :treatment and address ILIKE :address"
+      @treatments = Treatment.where(sql_query, treatment: "%#{params[:treatment]}%", address: "%#{params[:address]}%")
     else
       @treatments = Treatment.all
     end
