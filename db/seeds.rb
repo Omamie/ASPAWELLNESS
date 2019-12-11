c299bd0c5b5628d0183752b1adc88bc1e9c4d3ae
Paolo Pastacaldi <ppastacaldi@outlook.it>
2019-12-11 16:46:56 +0100

    added railsmoney gem and completed seed, except for PHOTO table

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,13 +55,13 @@ treatment_attributes = [{name:"Circulatory massage",description:"Targeted massag
 Treatment.create!(treatment_attributes)
 puts "Creating Bookings..."
 
-booking_attributes = [{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 2, start_time: "11/12/2019 15:00", end_time: "11/12/2019 18:00", status: 1},
-{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 3, start_time: "11/12/2019 15:00", end_time: "11/12/2019 18:00", status: 1},
-{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 2, start_time: "11/11/2019 15:00", end_time: "11/12/2019 16:00", status: 1},
-{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 2, start_time: "20/12/2019 15:00", end_time: "11/12/2019 17:00", status: 1},
-{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 1, start_time: "21/12/2019 07:00", end_time: "21/12/2019 08:00", status: 1},
-{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 5, start_time: "31/11/2019 17:00", end_time: "31/12/2019 18:00", status: 1},
-{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 4, start_time: "09/12/2019 10:00", end_time: "09/12/2019 11:00", status: 1}]
+booking_attributes = [{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 2, start_time: "11/12/2019 15:00", end_time: "11/12/2019 18:00", status: 1, price_cents: 2000},
+{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 3, start_time: "11/12/2019 15:00", end_time: "11/12/2019 18:00", status: 1, price_cents: 3000},
+{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 2, start_time: "11/11/2019 15:00", end_time: "11/12/2019 16:00", status: 1, price_cents: 1800},
+{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 2, start_time: "20/12/2019 15:00", end_time: "11/12/2019 17:00", status: 1, price_cents: 1200},
+{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 1, start_time: "21/12/2019 07:00", end_time: "21/12/2019 08:00", status: 1, price_cents: 6000},
+{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 5, start_time: "31/11/2019 17:00", end_time: "31/12/2019 18:00", status: 1, price_cents: 2000},
+{customer: Customer.all.sample,treatment_id:Treatment.ids.sample, group_size: 4, start_time: "09/12/2019 10:00", end_time: "09/12/2019 11:00", status: 1, price_cents: 1500}]
 
 
 Booking.create!(booking_attributes)
