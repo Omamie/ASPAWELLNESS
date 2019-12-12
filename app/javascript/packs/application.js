60da1762463afbe6d711320b4498b1c72b96b95f
Paolo Pastacaldi <ppastacaldi@outlook.it>
2019-12-12 17:17:38 +0100

    add calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,14 @@
+
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 
 import { initMapbox } from '../plugins/init_mapbox';
-import { flatpickr } from "../plugins/flatpickr"
+import { flatpickr } from "../plugins/flatpickr";
 
 
 initMapbox();
 flatpickr();
+
 
