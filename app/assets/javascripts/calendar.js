5685680eda56a71e2a4384e66576f8257efa54e6
mattia solarino <mattiasolarino@email.it>
2019-12-14 15:40:26 +0100

    Calendar modifications

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -6,11 +6,10 @@ if (element) {
 
   $('#calendar').fullCalendar({
     events: events,
+    defaultView: 'listWeek'
 
-  defaultView: 'listWeek'
   });
 }
-
 
 
 
@@ -22,7 +21,7 @@ $(document).ready(function() {
   todayDate.setHours(0,0,0,0);
 
   $('#calendar').fullCalendar({
-    editable: false,
+    editable: true,
     slotEventOverlap: false,
     monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Dicember'],
     monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dic'],
@@ -46,47 +45,8 @@ $(document).ready(function() {
       center: 'title',
       right: ''
     },
-    // firstDay: 1,
-    //this section is triggered when the event cell it's clicked
-    // selectable: true,
-    // selectHelper: true,
-    // select: function(start, end) {
-    //   var user_name;
-    //   user_name = prompt("User name: ");
-    //   var eventData;
-      //this validates that the user must insert a name in the input
-      // if (user_name) {
-      //   eventData = {
-      //     title: "Reserved",
-      //     start: start,
-      //     end: end,
-      //     user_name: user_name
-      //   };
-      //   //here i validate that the user can't create an event before today
-      //   if (eventData.start < todayDate){
-      //     alert("You can't choose a date that already past.");
-      //     $("#calendar").fullCalendar("unselect");
-      //     return
-      //   }
-      //   //if everything it's ok, then the event is saved in database with ajax
-      //   $.ajax({
-      //     url: "events",
-      //     type: "POST",
-      //     data: eventData,
-      //     dataType: 'json',
-      //     success: function(json) {
-      //       alert(json.msg);
-      //       $("#calendar").fullCalendar("renderEvent", eventData, true);
-      //       $("#calendar").fullCalendar("refetchEvents");
-      //     }
-      //   });
-      // }
-      // $("#calendar").fullCalendar("unselect");
-    // },
-    defaultView: 'agendaWeek',
-    allDaySlot: false,
+
     height: 500,
-    slotMinutes: 30,
     eventSources: [
       {
         url: '/events'
