83e996623032533d6ad6e818862250b5e87425c9
Paolo Pastacaldi <ppastacaldi@outlook.it>
2019-12-17 12:06:32 +0100

    fixed footer in category pages

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -36,7 +36,7 @@ class BookingsController < ApplicationController
         success_url: booking_url(@booking),
         cancel_url: new_treatment_booking_url(@treatment)
       )
-      @booking.update(checkout_session_id: session.id, status: "paid")
+      @booking.update(checkout_session_id: session.id)
       redirect_to new_booking_payment_path(@booking)
     else
 
