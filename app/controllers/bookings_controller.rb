b215e072879d88d2c791dc955186ce825f03a509
Omamerhi Etefia <omamerhie@gmail.com>
2019-12-17 10:54:20 +0100

    Fixed the payment journey - changed the status to pending if the booking is saved

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,8 +22,8 @@ class BookingsController < ApplicationController
     @booking.price = @treatment.price
     @booking.customer = current_user
     @user = current_user
+    @user.phone_number = @booking.phone_number
     if @booking.save
-      @user.phone_number = @booking.phone_number
       # Stripe Session
       session = Stripe::Checkout::Session.create(
         payment_method_types: ['card'],
@@ -36,7 +36,7 @@ class BookingsController < ApplicationController
         success_url: booking_url(@booking),
         cancel_url: new_treatment_booking_url(@treatment)
       )
-      @booking.update(checkout_session_id: session.id, status: "paid")
+      @booking.update(checkout_session_id: session.id, status: "pending")
       redirect_to new_booking_payment_path(@booking)
     else
 
